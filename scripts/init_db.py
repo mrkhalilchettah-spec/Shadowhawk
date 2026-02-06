55d3c0d9f067349479e4c46661e33d3f6f10fc24
khalil chettah <mr.khalilchettah@gmail.com>
2026-02-07 00:36:08 +0100

    Update copyright and license information in init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -2,8 +2,8 @@
 """
 ShadowHawk Platform - Database Initialization Script
 
-Copyright (c) 2024 ShadowHawk Platform
-Licensed under the MIT License
+Copyright (c) 2026 ShadowHawk Platform
+Licensed under the Apache License
 See LICENSE file in the project root for full license information.
 """
 
